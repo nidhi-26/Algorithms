f8f21bf8f678b3885e6e1ea0466fd9e480fa0c36
nidhi-26 <32599084+nidhi-26@users.noreply.github.com>
2019-06-05 18:01:34 +0530

    Add files via upload
    Calculate number of 1's in the binary representation of an integer

---
+++ NumberOfOnesInBinary.cpp
@@ -0,0 +1,26 @@
+/*
+	Author: Nidhi Bothra
+	Created On: 2019-06-05
+	Description:	Calculate number of 1's in the binary representation of an integer
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int onesInBinary(int n) {
+	int sum = 0;
+	while (n>0) {
+		sum += n & 1;
+		n >>= 1;
+	}
+}
+
+int main() {
+	int n, NoOfOnes = 0;
+	cout<<"Enter a number: ";
+	cin>>n;
+	NoOfOnes = onesInBinary(n);
+	cout<<"Number of 1's in binary representation of "<<n<<" is "<<NoOfOnes<<endl;
+	return 0;
+}
